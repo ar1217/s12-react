12014e9e364340b20d76c1a43eae79d17da0f401
Andrea Rubino <andre.utnfrt@gmail.com>
2022-09-15 12:36:29 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [color, setColor] = useState('#000000')
+  const [intervalo, setIntervalo] = useState(0);
+
+  const obtenerColorAleatorio = () => {   
+    const hex = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    return setColor(hex)    
+  }
+
+  const cambiarColor = () => {
+    return setIntervalo(setInterval(obtenerColorAleatorio, 500))
+
+  }
+
+  const detenerCambio = () => {
+    return clearInterval(intervalo);
+  }
+
+  const cambiarConClick = () => {
+    return clearInterval(intervalo);
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: '255px', height: '255px', backgroundColor: color }} id='cuadrado' onMouseOver={cambiarColor} onMouseLeave={detenerCambio} onDoubleClick={cambiarConClick}>
+        <p style={{color:'white', fontWeight:'bold' }}>{color}</p>
+      </div>
     </div>
   );
 }
